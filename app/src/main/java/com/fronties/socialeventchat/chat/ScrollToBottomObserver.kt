b0378ec3012972b95108067acc13ce1c45fab368
Aavash <aavashsthapit@gmail.com>
2021-09-18 23:28:42 -0500

    First commit

---
+++ app/src/main/java/com/fronties/socialeventchat/chat/ScrollToBottomObserver.kt
@@ -0,0 +1,25 @@
+package com.fronties.socialeventchat.chat
+
+import android.widget.Adapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ScrollToBottomObserver(
+    private val recyclerView: RecyclerView,
+    private val adapter: Adapter, // TODO: change to whichever adapter is added later
+    private val manager: LinearLayoutManager
+) : RecyclerView.AdapterDataObserver() {
+    override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+        super.onItemRangeInserted(positionStart, itemCount)
+//        val count = adapter.itemCount
+        val lastVisiblePosition = manager.findLastCompletelyVisibleItemPosition()
+        // If the recycler view is initially being loaded or the
+        // user is at the bottom of the list, scroll to the bottom
+        // of the list to show the newly added message.
+//        val loading = lastVisiblePosition == -1
+//        val atBottom = positionStart >= count - 1 && lastVisiblePosition == positionStart - 1
+//        if (loading || atBottom) {
+//            recyclerView.scrollToPosition(positionStart)
+//        }
+    }
+}
---
+++ app/src/main/java/com/fronties/socialeventchat/chat/model/Message.kt
@@ -0,0 +1,10 @@
+package com.fronties.socialeventchat.chat.model
+
+data class Message(
+    val uid: String? = null,
+    val text: String? = null,
+    val name: String? = null,
+    val photoUrl: String? = null,
+    val imageUrl: String? = null,
+    val timeStamp: String? = null
+)
