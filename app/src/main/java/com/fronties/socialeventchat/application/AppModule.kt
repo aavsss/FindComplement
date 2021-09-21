cd58287c6fbb92f2c67750901a0649bc56de4782
Aavash <aavashsthapit@gmail.com>
2021-09-21 00:17:10 -0500

    Added retrofit instance in DI

--- app/src/main/java/com/fronties/socialeventchat/application/AppModule.kt
+++ app/src/main/java/com/fronties/socialeventchat/application/AppModule.kt
@@ -6,11 +6,14 @@ import com.bumptech.glide.RequestManager
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.fronties.socialeventchat.R
+import com.fronties.socialeventchat.chat.api.ChatApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -27,4 +30,12 @@ object AppModule {
             .error(R.drawable.ic_launcher_foreground)
             .diskCacheStrategy(DiskCacheStrategy.DATA)
     )
+
+    @Singleton
+    @Provides
+    fun provideRetroApiInstance(): ChatApi = Retrofit.Builder()
+        .baseUrl("baseURL") // TODO put the url hosted
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ChatApi::class.java)
 }
