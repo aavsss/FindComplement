9d68ce94e888ad227e1cc826da97bf4a3b9bba38
Aavash <aavashsthapit@gmail.com>
2021-09-25 13:15:40 -0500

    adding content layout

--- app/src/main/java/com/fronties/socialeventchat/MainActivity.kt
+++ app/src/main/java/com/fronties/socialeventchat/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fronties.socialeventchat
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fronties.socialeventchat.databinding.ActivityMainBinding
+import com.fronties.socialeventchat.databinding.FragmentEventDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,10 +11,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var tempBinding: FragmentEventDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-        setContentView(view)
+
+        //region test
+        tempBinding = FragmentEventDetailBinding.inflate(layoutInflater)
+        val tempView = tempBinding.root
+
+//        setSupportActionBar(tempBinding.toolbar)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        supportActionBar?.setDisplayShowHomeEnabled(true)
+//        tempBinding.toolbarLayout.title = title
+        //endregion
+
+        setContentView(tempView)
     }
 }
