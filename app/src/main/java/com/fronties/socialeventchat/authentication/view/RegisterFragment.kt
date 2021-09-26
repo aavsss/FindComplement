c333513ffab04265c69b74b2229e7c86cdbb9829
cvam77 <schaudhary@patriots.uttyler.edu>
2021-09-26 01:46:07 -0500

    login and register fragment created

---
+++ app/src/main/java/com/fronties/socialeventchat/authentication/view/RegisterFragment.kt
@@ -0,0 +1,24 @@
+package com.fronties.socialeventchat.authentication.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fronties.socialeventchat.R
+
+class RegisterFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+}
