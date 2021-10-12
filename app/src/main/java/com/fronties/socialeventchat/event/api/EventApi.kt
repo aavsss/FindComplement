416ab747567582aabaf43998591bf5824b7921e1
Aavash <aavashsthapit@gmail.com>
2021-10-12 01:26:13 -0500

    Base API for Events

--- app/src/main/java/com/fronties/socialeventchat/event/api/EventApi.kt
+++ app/src/main/java/com/fronties/socialeventchat/event/api/EventApi.kt
@@ -1,4 +1,32 @@
 package com.fronties.socialeventchat.event.api
 
+import com.fronties.socialeventchat.event.model.EventResponse
+import com.fronties.socialeventchat.event.model.SocialEvents
+import retrofit2.Response
+import retrofit2.http.*
+
 interface EventApi {
+
+    @GET()
+    suspend fun getEventsList(): Response<List<SocialEvents>>
+
+    @GET()
+    suspend fun getEventDetails(
+        @Path("eid") eid: Int
+    ): Response<SocialEvents>
+
+    @POST()
+    suspend fun addEvent(
+        @Body event: SocialEvents
+    ): Response<EventResponse>
+
+    @PUT()
+    suspend fun editEvent(
+        @Body event: SocialEvents
+    ): Response<EventResponse>
+
+    @DELETE()
+    suspend fun deleteEvent(
+        @Path("eid") eid: Int
+    ): Response<EventResponse>
 }
