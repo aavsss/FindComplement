4c9412fbc3af3b47b05decea24a0d46d3c1870ea
Aavash <aavashsthapit@gmail.com>
2021-10-16 16:57:12 -0500

    merged bottom nav

--- app/src/main/java/com/fronties/socialeventchat/event/eventList/EventListFragment.kt
+++ app/src/main/java/com/fronties/socialeventchat/event/eventList/EventListFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.fronties.socialeventchat.R
 import com.fronties.socialeventchat.databinding.FragmentEventListBinding
 
