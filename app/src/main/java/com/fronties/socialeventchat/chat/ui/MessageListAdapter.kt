9c9a579392094a53a9b27d9ca06cfe118a5dadad
Aavash <aavashsthapit@gmail.com>
2021-10-22 20:59:15 -0500

    Base layout copied

---
+++ app/src/main/java/com/fronties/socialeventchat/chat/ui/MessageListAdapter.kt
@@ -0,0 +1,8 @@
+package com.fronties.socialeventchat.chat.ui
+
+import androidx.recyclerview.widget.RecyclerView
+
+//class MessageListAdapter: RecyclerView.Adapter {
+//
+//    private class SentMessageHolder:
+//}
